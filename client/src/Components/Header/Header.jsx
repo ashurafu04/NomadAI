73824807d59296108be83035be32c590e9868f61
bilal-essafrioui <bilal.essafrioui@gmail.com>
2025-03-18 15:55:42 +0000

    updated navbar

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,20 +1,3 @@
-/*import { Link } from "react-router-dom";
-import "./Header.css";*/
-
-/*const Header = () => {
-  return (
-    <header className="header">
-      <h1 className="logo">NomadAI</h1>
-      <nav className="nav">
-        <Link to="/" className="link">Home</Link>
-        <Link to="/nomadAi" className="link">Talk To Nomad</Link>
-        <Link to="/features" className="link">Coming Soon</Link>
-        <Link to="/contact" className="link">Contact</Link>
-      </nav>
-      <button className="search-button">Search</button>
-    </header>
-  );
-};*/
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
@@ -46,6 +29,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-//export default Header;
